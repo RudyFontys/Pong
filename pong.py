702d71a8782d40377bad81c4f6e74a6acf6b4407
RudyFontys <r.bouland@fontys.nl>
2026-03-08 11:49:50 +0100

    Testen algoritmische patronen met een loop

--- pong.py
+++ pong.py
@@ -5,3 +5,11 @@ color('pink')
 forward(100)
 home()
 wait = input("Press Enter to continue.")
+clearscreen()
+# Algoritmische patronen met loops:
+for steps in range(100):
+    for c in ('red', 'green', 'blue'):
+        color(c)
+        forward(steps)
+        right(30)
+wait = input("Press Enter to continue.")
